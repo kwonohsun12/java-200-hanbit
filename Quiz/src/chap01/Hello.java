5e62bbeba31ad7458f6ac2cc7b6ca57ef03ec4e2
OhSun Kwon <84365905+kwonohsun12@users.noreply.github.com>
2021-11-19 17:29:24 +0900

    update

--- Quiz/src/chap01/Hello.java
+++ Quiz/src/chap01/Hello.java
@@ -1,11 +1,17 @@
-package chap01;
+package Chap01;
 
 public class Hello {
 	public static void main(String[] args) {
 		System.out.println("첫 java프로그램입니다.");
 		System.out.println("화면에 출력하고 있습니다.");
 		//주의! print에 사용한 l은 영문 소문자 l입니다.
-		
+		/*
+		 * 전통적 주석
+		 */
+		/**
+		 * 문서화 주석
+		 */
+		//한 줄 주석
 	}
 }
 
